c2c253858f854829c1a7e169a5f340b193f6f546
Benedek5@stud.uni-obuda.hu <szbeni2001@gmail.com>
2026-03-13 19:58:13 +0100

    Vote model added

---
+++ Backend/NeighboutrHub/Entities/Models/Vote.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Vote : IIdEntity
+    {
+        public Vote()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        [StringLength(50)]
+        public string Id { get; set; }
+
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime Deadline { get; set; }
+
+        [StringLength(50)]
+        public string CreatedByUserId { get; set; } = string.Empty;
+
+        //public User CreatedByUser { get; set; } = null!;
+
+     
+    }
+}
+
