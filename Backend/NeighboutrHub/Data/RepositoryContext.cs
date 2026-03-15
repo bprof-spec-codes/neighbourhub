890e7f20207315d9c19b274dd8df1a4009da3d6e
hanuszbettina <hanuszbettina.sulis@gmail.com>
2026-03-15 13:32:10 +0100

    Save lists as JSON text in a single column.

--- Backend/NeighboutrHub/Data/RepositoryContext.cs
+++ Backend/NeighboutrHub/Data/RepositoryContext.cs
@@ -15,5 +15,27 @@ public class RepositoryContext : IdentityDbContext<AppUser>
     {
         
     }
-    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        var listConverter = new ValueConverter<List<string>, string>(
+        v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
+        v => JsonSerializer.Deserialize<List<string>>(v, (JsonSerializerOptions)null) ?? new List<string>()
+    );
+
+        var listComparer = new ValueComparer<List<string>>(
+            (c1, c2) => c1.SequenceEqual(c2),
+            c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+            c => c.ToList()
+        );
+
+        foreach (var property in new[] { "Storage", "ApartmentNumber", "ParkingSpace" })
+        {
+            builder.Entity<AppUser>()
+                .Property(property)
+                .HasConversion(listConverter)
+                .Metadata
+                .SetValueComparer(listComparer);
+        }
+    }
 }
