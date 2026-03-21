dc1eb0c51b7d24a67cc7675fd1332c1b160f685b
Nagy Zsombor <nzsombor04@gmail.com>
2026-03-21 15:12:59 +0100

    added html and style for announcements list

--- Frontend/neighbourhub/src/app/components/announcement-list/announcement-list.component.ts
+++ Frontend/neighbourhub/src/app/components/announcement-list/announcement-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AnnouncementService } from '../../services/announcement.service';
+import { Observable } from 'rxjs';
+import { Announcement } from '../../entities/models/announcement.model';
+import { AnnouncementCategory } from '../../entities/enums/announcement-category.model';
 
 @Component({
   selector: 'app-announcement-list',
@@ -8,10 +11,58 @@ import { AnnouncementService } from '../../services/announcement.service';
   styleUrl: './announcement-list.component.scss'
 })
 export class AnnouncementListComponent implements OnInit {
+  protected announcements$ = new Observable<Announcement[]>();
 
   constructor(private announcementService: AnnouncementService) { }
 
   public ngOnInit(): void {
+    this.loadAnnouncements();
+  }
+
+  private loadAnnouncements(): void {
     this.announcementService.loadAnnouncements();
+    this.announcements$ = this.announcementService.announcements$;
+  }
+
+  protected addAnnouncement(): void {
+    alert('Add Announcement functionality is not implemented yet.');
+  }
+
+  protected getCategoryText(category: Announcement['category']): string {
+    if (typeof category === 'number') {
+      return AnnouncementCategory[category] ?? 'General';
+    }
+
+    return String(category);
+  }
+
+  protected getCategoryBadgeClass(category: Announcement['category']): string {
+    const categoryText = this.getCategoryText(category).toLowerCase();
+
+    switch (categoryText) {
+      case 'maintenance':
+        return 'badge-maintenance';
+      case 'event':
+        return 'badge-event';
+      case 'policy':
+        return 'badge-policy';
+      default:
+        return 'badge-general';
+    }
+  }
+
+  protected getCategoryCardClass(category: Announcement['category']): string {
+    const categoryText = this.getCategoryText(category).toLowerCase();
+
+    switch (categoryText) {
+      case 'maintenance':
+        return 'category-maintenance';
+      case 'event':
+        return 'category-event';
+      case 'policy':
+        return 'category-policy';
+      default:
+        return 'category-general';
+    }
   }
 }
