2ae1b087e4fb4acc90953d5f8f386126b2360942
Benedek5@stud.uni-obuda.hu <szbeni2001@gmail.com>
2026-03-23 13:51:14 +0100

    Add CreateVoteDto, update VoteLogic with Create method and DtoProvider

--- Backend/NeighboutrHub/Entities/Dtos/Vote/CreateVoteDto.cs
+++ Backend/NeighboutrHub/Entities/Dtos/Vote/CreateVoteDto.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entities.Dtos.Vote
 {
-    internal class CreateVoteDto
+    public class CreateVoteDto
     {
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime Deadline { get; set; }
+
     }
 }
--- Backend/NeighboutrHub/Logic/Logic/VoteLogic.cs
+++ Backend/NeighboutrHub/Logic/Logic/VoteLogic.cs
@@ -1,13 +1,14 @@
 ﻿using Data;
 using Entities.Dtos.Vote;
-using Entities.Models;
+using Entities.Enums;
 using Entities.Helpers;
+using Entities.Models;
+using Logic.Helper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Entities.Enums;
 
 namespace Logic.Logic
 {
@@ -15,10 +16,12 @@ namespace Logic.Logic
     {
 
         private readonly Repository<Vote> voteRepository;
+        private readonly DtoProvider dtoProvider;
 
-        public VoteLogic(Repository<Vote> voteRepository)
+        public VoteLogic(Repository<Vote> voteRepository, DtoProvider dtoProvider)
         {
             this.voteRepository = voteRepository;
+            this.dtoProvider = dtoProvider;
         }
 
         private VoteDto ToDto(Vote vote)
@@ -53,6 +56,17 @@ namespace Logic.Logic
             return voteRepository.GetAll()
                 .Where(v => v.Deadline <= DateTime.Now)
                 .Select(ToDto);
+        }
+
+        public VoteDto Create(CreateVoteDto dto, string createdByUserId)
+        {
+            if (dto.Deadline <= DateTime.Now)
+                throw new ArgumentException("A deadline jövőbeli dátum kell legyen.");
+
+            var vote = dtoProvider.Mapper.Map<Vote>(dto);
+            vote.CreatedByUserId = createdByUserId;
+            voteRepository.Add(vote);
+            return ToDto(vote);
         }
 
         public void Delete(string id)
