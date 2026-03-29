1550b2a5136f0875c5a4830fba60ee122389b9c2
hanuszbettina <hanuszbettina.sulis@gmail.com>
2026-03-29 17:27:58 +0200

    ApproveAppUserDto created

---
+++ Backend/NeighboutrHub/Entities/Dtos/User/ApproveAppUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.User
+{
+    public class ApproveAppUserDto
+    {
+        public required string UserId { get; set; }
+        public required string Role { get; set; } // Pl: "OwnerResiding", "Tenant", "Owner"
+    }
+}
