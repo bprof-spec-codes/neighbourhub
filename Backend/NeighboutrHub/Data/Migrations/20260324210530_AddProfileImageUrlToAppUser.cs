6d77fc26a20d1b168325810a47974551741b8736
Máté Zsebeházi <zsebehazimate@gmail.com>
2026-03-29 14:03:30 +0200

    Add ProfileImageUrl property to AppUser model

---
+++ Backend/NeighboutrHub/Data/Migrations/20260324210530_AddProfileImageUrlToAppUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfileImageUrlToAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(2048)",
+                maxLength: 2048,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
