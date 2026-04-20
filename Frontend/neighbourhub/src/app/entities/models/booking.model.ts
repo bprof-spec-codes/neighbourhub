eca326564ed429b30f71fef9953f7eefab9c8aab
Tolnai Levente <levi.tolnai7@gmail.com>
2026-04-21 00:50:33 +0200

    add Booking and CommunityRoom models and DTOs

---
+++ Frontend/neighbourhub/src/app/entities/models/booking.model.ts
@@ -0,0 +1,10 @@
+export interface BookingListItem {
+  id: string;
+  roomName: string;
+  bookedByName: string;
+  bookingDate: string;
+  startTime: string;
+  endTime: string;
+  status: string;
+  numberOfPeople: number;
+}
