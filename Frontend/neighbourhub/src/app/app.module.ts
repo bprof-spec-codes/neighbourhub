7c947285a21ce9d0a7de1dee77fe7b4373311d7f
Tolnai Levente <levi.tolnai7@gmail.com>
2026-05-03 21:57:01 +0200

    add IssueCommentModal component

--- Frontend/neighbourhub/src/app/app.module.ts
+++ Frontend/neighbourhub/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { MessageViewModalComponent } from './components/messaging/message-view-m
 import { BookingsComponent } from './components/bookings/bookings.component';
 import { BookingCreateModalComponent } from './components/bookings/booking-create-modal/booking-create-modal.component';
 import { RoomManageModalComponent } from './components/bookings/room-manage-modal/room-manage-modal.component';
+import { IssueCommentModalComponent } from './components/issues/issue-comment-modal/issue-comment-modal.component';
 
 @NgModule({
   declarations: [
@@ -59,7 +60,8 @@ import { RoomManageModalComponent } from './components/bookings/room-manage-moda
     PendingUsersComponent,
     MessagingComponent,
     MessageNewModalComponent,
-    MessageViewModalComponent
+    MessageViewModalComponent,
+    IssueCommentModalComponent
   ],
   imports: [
     BrowserModule,
